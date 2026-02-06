eae8d887163408ac198c2f5a4b405d61f3154b66
Varun <varunkumar.pinnam@gmail.com>
2026-02-06 17:36:58 +0530

    updated varaiables templates

--- 2-filecount.sh
+++ 2-filecount.sh
@@ -16,10 +16,12 @@ if [ -s $FILE_NAME ]; then
     echo -e "$R $FILE_NAME is an empty file $N"
     
     elif [ -f "$FILE_NAME" ]; then  
-    echo -e "$Y The number of lines in $G $FILE_NAME: $(wc -l $FILE_NAME) $N"
+    echo -e "$Y The number of lines in $G $FILE_NAME: $(wc -l) $N"
     echo -e "$Y The number of words in $G $FILE_NAME: $(wc -w $FILE_NAME) $N"
     echo -e "$Y The number of characters in $G $FILE_NAME: $(wc -m $FILE_NAME) $N"
 
     else
             echo -e "$R $FILE_NAME is not available $N"
 fi
+
+ The number of lines in  a.txt: 0 a.txt
