735f5477aea1c93f0d37423e46e6a85a9579479b
Varun <varunkumar.pinnam@gmail.com>
2026-02-06 18:48:43 +0530

    updated varaiables templates

--- 3-string_replacement.sh
+++ 3-string_replacement.sh
@@ -13,6 +13,6 @@ if [ -d $DIR ]; then
     find $DIR -type f -name "*.conf" -execu sed "s/$OLD/$NEW/g" {} +
     exit 0
 else
-    echo "%DIR is not available
-    exit
+    echo "%DIR is not available"
+    exit 1
 fi
