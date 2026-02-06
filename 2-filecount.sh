635a83f060d38039320e8675fd9637d6d8bbdbdc
Varun <varunkumar.pinnam@gmail.com>
2026-02-06 18:01:00 +0530

    updated varaiables templates

--- 2-filecount.sh
+++ 2-filecount.sh
@@ -12,14 +12,42 @@ usage(){
 
 [ $# -lt 1 ] && usage;
 
-if [[ -f "$FILE_NAME" && ! -s "$FILE_NAME" ]]; then
+if [ ! -f $FILE_NAME ]; then
+        echo -e "$R $FILE_NAME is not available $N"
+        exit 1
+    elif [ ! -s $FILE_NAME ]; then
+            echo -e "$G $FILE_NAME exists $R but its an empty file $N"
+            exit 0
+    elif [-f "$FILE_NAME" ]; then
+            echo -e "$Y The number of lines in $G $FILE_NAME: $(wc -l < "$FILE_NAME") $N"
+            echo -e "$Y The number of words in $G $FILE_NAME: $(wc -w < "$FILE_NAME") $N"
+            echo -e "$Y The number of characters in $G $FILE_NAME: $(wc -m < "$FILE_NAME") $N"
+            exit 0
+    else 
+        echo " file is not a regular file "
+fi 
 
-    echo -e "$Y The number of lines in $G $FILE_NAME: $(wc -l < "$FILE_NAME") $N"
-    echo -e "$Y The number of words in $G $FILE_NAME: $(wc -w < "$FILE_NAME") $N"
-    echo -e "$Y The number of characters in $G $FILE_NAME: $(wc -m < "$FILE_NAME") $N"
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if [[ -f "$FILE_NAME" && -s "$FILE_NAME" ]]; then
+
+#     echo -e "$Y The number of lines in $G $FILE_NAME: $(wc -l < "$FILE_NAME") $N"
+#     echo -e "$Y The number of words in $G $FILE_NAME: $(wc -w < "$FILE_NAME") $N"
+#     echo -e "$Y The number of characters in $G $FILE_NAME: $(wc -m < "$FILE_NAME") $N"
        
-      else
-            echo -e "$R $FILE_NAME is not available/empty $N"
-fi
+#       else
+#             echo -e "$R $FILE_NAME is not available/empty $N"
+# fi
 
  
