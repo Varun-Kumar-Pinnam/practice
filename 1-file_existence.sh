33667d750fb172033b8e5b1765656288fe574785
Varun <varunkumar.pinnam@gmail.com>
2026-02-06 16:52:51 +0530

    updated varaiables templates

--- 1-file_existence.sh
+++ 1-file_existence.sh
@@ -21,8 +21,8 @@ if [ -f "$FILE_NAME" ]; then
     # fi
 #...checking permission level
 
-    [ -r "$FILE_NAME" ] && echo "$FILE_NAME is readable || echo "$FILE_NAME is not readable 
-    [ -w $FILE_NAME ] && echo "$FILE_NAME is writable || echo "$FILE_NAME is not writable 
+    [ -r "$FILE_NAME" ] && echo "$FILE_NAME" is readable || echo "$FILE_NAME" is not readable 
+    [ -w "$FILE_NAME" ] && echo "$FILE_NAME" is writable || echo "$FILE_NAME" is not writable 
 
 else
     echo "$FILE_NAME is not available"
