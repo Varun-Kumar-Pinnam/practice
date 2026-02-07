dfc43637df52b1f88cd336882f9af3e847f2db32
Varun <varunkumar.pinnam@gmail.com>
2026-02-07 15:15:29 +0530

    updated varaiables templates

--- 8-primenumber.sh
+++ 8-primenumber.sh
@@ -15,8 +15,15 @@ if  [ $# -lt 1 ]; then
     elif [[ ! $input =~ ^[0-9]+$ ]]; then
             echo -e "Input $R "$input" $N must be a number"
             exit 1
-    elif (( (input > 1) && (input/input !=1) ));then 
-            echo "$input is a prime number"
-     
-
+    elif [[ input <= 1 ]]; then
+            echo -e "$input is not a prime member"
+            exit 1
 fi
+
+for (( i=2; i*i<=num; i++ ));do 
+    if (( num % i == 0 )); then 
+        echo "$Y $input is not a prime number $N"
+        exit 0
+done
+
+echo "$G $input $Y is prime number $N"
