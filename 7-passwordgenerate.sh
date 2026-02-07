f41040769deb623743706cdf8340f64ac5206813
Varun <varunkumar.pinnam@gmail.com>
2026-02-07 13:58:56 +0530

    updated varaiables templates

--- 7-passwordgenerate.sh
+++ 7-passwordgenerate.sh
@@ -5,39 +5,42 @@ usage(){
     echo "USAGE: sh $0 <password length>"
     exit 1
 }
+ 
+if  [ $# -lt 1 ]; then 
+        usage 
+    elif [ ! "$length" =~ ^[0-9]+$ ]; then
+            echo "Password input $length must be a number"
+            exit 1
+    else [ ! "$length" -ge 10 ]; then 
+            echo "password length should be 10 characters"
+            exit 1
+fi
 
+echo "$lenght"
 
-[ $# -lt 1 ] && usage #echo "USAGE: sh $0 <password length>"
+# # Character sets 
+#     UPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#     LOWER="abcdefghijklmnopqrstuvwxyz"
+#     NUMBERS="0123456789"
+#     SPECIAL="!@#&$"
 
+# # one charcater from each set
+# password="$(
+#     echo "${UPPER:RANDOM%${#UPPER}:1}"
+#     echo "${LOWER:RANDOM%${#LOWER}:1}"
+#     echo "${NUMBER:RANDOM%${#NUMBER}:1}"
+#     echo "${SPECIAL:RANDOM%${#SPECIAL}:1}"
+# )"
 
-[ "$length" -lt 10 ] && echo "password should be 10 characters"
-exit 1
+# #remaining characters
+# remaining="$($length -4)"
 
+# ALL="$UPPER$LOWER$NUMBER$SPECIAL"
 
-
-# Character sets 
-    UPPER="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    LOWER="abcdefghijklmnopqrstuvwxyz"
-    NUMBERS="0123456789"
-    SPECIAL="!@#&$"
-
-# one charcater from each set
-password="$(
-    echo "${UPPER:RANDOM%${#UPPER}:1}"
-    echo "${LOWER:RANDOM%${#LOWER}:1}"
-    echo "${NUMBER:RANDOM%${#NUMBER}:1}"
-    echo "${SPECIAL:RANDOM%${#SPECIAL}:1}"
-)"
-
-#remaining characters
-remaining="$($length -4)"
-
-ALL="$UPPER$LOWER$NUMBER$SPECIAL"
-
-for(( i=0; i<$remaining; i++ ));
-    do
-    password+="${ALL:RANDOM%${#ALL}:1}"
-    done
+# for(( i=0; i<$remaining; i++ ));
+#     do
+#     password+="${ALL:RANDOM%${#ALL}:1}"
+#     done
 
 
 
