4bbfbeea5e463a424bbf5ee8d0aa080f3ecb7745
Varun <varunkumar.pinnam@gmail.com>
2026-02-07 14:08:22 +0530

    updated varaiables templates

--- 7-passwordgenerate.sh
+++ 7-passwordgenerate.sh
@@ -43,7 +43,7 @@ echo "$REMAINING"
 
 ALL="$UPPER$LOWER$NUMBER$SPECIAL"
 
-for(( i=0; i<$remaining; i++ ));
+for(( i=0; i<"$remaining"; i++ ));
     do
     PASSWORD+="${ALL:RANDOM%${#ALL}:1}"
     done
