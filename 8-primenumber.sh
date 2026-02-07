5edb761aafa4f961d6e8dfabd94c166cb780c544
Varun <varunkumar.pinnam@gmail.com>
2026-02-07 14:59:30 +0530

    updated varaiables templates

---
+++ 8-primenumber.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+input=$1
+
+usage(){
+    echo -e "$Y USAGE: sh $0 <Number> $N"
+    exit 1
+}
+ 
+if  [ $# -lt 1 ]; then 
+        usage 
+    elif [[ ! "$input" =~ ^[0-9]+$ ]]; then
+            echo -e "Input $R "$length" $N must be a number"
+            exit 1
+    elif (( (input > 1) && (input/input !=1) ));then 
+            echo "$input is a prime number"
+     
+
+fi
