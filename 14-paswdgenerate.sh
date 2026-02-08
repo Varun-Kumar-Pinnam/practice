676a3b3e05bef170c9a5faa7d302d56e3db2b23f
Varun Kumar Pinnam <VarunKumar.Pinnam@csgi.com>
2026-02-08 17:27:06 +0530

    practice

--- 14-paswdgenerate.sh
+++ 14-paswdgenerate.sh
@@ -5,7 +5,7 @@ length=$1
 if [[ ! $max =~ ^[0-9]+$ ]]; then 
         echo "please input only numeric value"
         exit 1
-    elif [[ $length -lt 10]];then 
+    elif [[ $length -lt 10]]; then 
         echo "Password length should be 10 characters"
         exit 1
 fi 
