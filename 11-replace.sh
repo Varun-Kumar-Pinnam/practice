1054d8741695a06c8ec4876cbecda9758e2dbb68
Varun Kumar Pinnam <VarunKumar.Pinnam@csgi.com>
2026-02-08 13:14:25 +0530

    practice

---
+++ 11-replace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+OLD=FOO
+NEW=BAR
+DIR=$1
+
+usage(){
+    echo "USAGE: sh $0 <DIR_NAME>"
+    exit 1
+}
+
+[ $# -lt 1 ] && 
+
+if [ -d $DIR ]; then
+    find $DIR -type f -name "*.conf" -exec sed -i.bak 's/$OLD/$NEW/g' {} +
+    exit 0
+    else 
+        echo "$DIR is not available"
+        exit 1
+fi
