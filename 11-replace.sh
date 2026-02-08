d655369d24314f911a3971031d075108899a6cdd
Varun Kumar Pinnam <VarunKumar.Pinnam@csgi.com>
2026-02-08 13:17:23 +0530

    practice

--- 11-replace.sh
+++ 11-replace.sh
@@ -12,7 +12,8 @@ usage(){
 [ $# -lt 1 ] && usage
 
 if [ -d $DIR ]; then
-    find $DIR -type f -name "*.conf" -exec sed -i.bak 's/$OLD/$NEW/g' {} +
+    find $DIR -type f -name "*.conf" -exec sed -i.bak "s/$OLD/$NEW/g" {} +
+    #find $DIR -type f -name "*.conf" -exec sed -i.bak "s/$OLD/$NEW/g" {} +
     exit 0
     else 
         echo "$DIR is not available"
