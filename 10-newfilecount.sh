1372444c21a7f68e946f8f3e2697b589f6ec2431
Varun Kumar Pinnam <VarunKumar.Pinnam@csgi.com>
2026-02-08 12:50:38 +0530

    practice

--- 10-newfilecount.sh
+++ 10-newfilecount.sh
@@ -9,23 +9,23 @@ usage(){
 
 [ $# -lt 1 ] && usage;
 
-# if [ ! -e $FILE_NAME ]; then
-#         echo "$FILE_NAME is not exists"
-#         exit 1
-#     elif [ ! -r $FILE_NAME ]; then
-#         echo"$FILE_NAME is not accessable"
-#         exit 1
-#     elif [ ! -s $FILE_NAME ]; then 
-#         echo "$FILE_NAME is an empty file"
-#         exit 0
-#     elif [ -f $FILE_NAME ]; then 
-#         echo "The number of line in $FILE_NAME: $(wc -l < $FILE_NAME)"
-#         echo "The number of words in $FILE_NAME: $(wc -w < $FILE_NAME)"
-#         echo "The number of characters in $FILE_NAME: $(wc -m < $FILE_NAME)"
-#         exit 0
-#     else 
-#         echo "$FILE_NAME is not a regular file"
-# fi
+if [ ! -e $FILE_NAME ]; then
+        echo "$FILE_NAME is not exists"
+        exit 1
+    elif [ ! -r $FILE_NAME ]; then
+        echo"$FILE_NAME is not accessable"
+        exit 1
+    elif [ ! -s $FILE_NAME ]; then 
+        echo "$FILE_NAME is an empty file"
+        exit 0
+    elif [ -f $FILE_NAME ]; then 
+        echo "The number of line in $FILE_NAME: $(wc -l < $FILE_NAME)"
+        echo "The number of words in $FILE_NAME: $(wc -w < $FILE_NAME)"
+        echo "The number of characters in $FILE_NAME: $(wc -m < $FILE_NAME)"
+        exit 0
+    else 
+        echo "$FILE_NAME is not a regular file"
+fi
 
 
-
+echo "$FILE_NAME"
