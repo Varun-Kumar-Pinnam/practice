7c4e9739fb37d1e6343e3bf2095306d9db52f4e4
Varun Kumar Pinnam <VarunKumar.Pinnam@csgi.com>
2026-02-08 14:18:28 +0530

    practice

---
+++ 12-csvfilter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+INPUT_FILE=$1
+OUTPUT_FILE=ERROR.txt
+
+usage(){
+    echo "USAGE: sh $0 <DIR_NAME>"
+    exit 1
+}
+
+[ $# -lt 1 ] && usage
+
+if [ $INPUT_FILE == "*.csv" ]; then 
+        awk -F ',' 'NR==1 || /ERROR/' "$INPUT_FILE" >> $$OUTPUT_FILE
+        exit o
+        else
+            echo "$INPUT_FILe is not a csv file"
+            exit 1
+fi
